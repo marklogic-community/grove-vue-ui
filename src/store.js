384fe7a1fd184990301f7fe82319f82d08c450c7
Geert Josten <geert.josten@marklogic.com>
2018-06-24 19:49:45 +0200

    Refactored vue store for multiple search and crud states

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-import authApi from "./api/AuthApi";
-import searchApi from "./api/SearchApi";
-import crudApi from "./api/CRUDApi";
+import authApi from './api/AuthApi';
+import searchApi from './api/SearchApi';
+import crudApi from './api/CRUDApi';
 
 Vue.use(Vuex);
 
@@ -15,58 +15,57 @@ const auth = {
     initialized: false,
     authenticated: false,
     username: undefined,
-    password: undefined,
     profile: undefined
   },
   mutations: {
-    loggedIn(state, { username, password }) {
+    isInitialized(state, { initialized }) {
+      state.initialized = initialized;
+    },
+    isAuthenticated(state, { authenticated }) {
+      state.authenticated = authenticated;
+      if (!authenticated) {
+        state.username = undefined;
+        state.profile = undefined;
+      }
+    },
+    loggedIn(state, { username }) {
       state.authenticated = true;
       state.username = username;
-      state.password = password;
     },
     loggedOut(state) {
       state.authenticated = false;
       state.username = undefined;
-      state.password = undefined;
       state.profile = undefined;
     },
-    gotStatus(state, { authenticated }) {
-      state.initialized = true;
-      state.authenticated = authenticated;
-      if (!authenticated) {
-        state.username = undefined;
-        state.password = undefined;
-        state.profile = undefined;
-      }
-    },
-    gotProfile(state, { profile }) {
-      state.profile = profile || {};
-    },
-    updatedProfile(state, { profile }) {
+    setProfile(state, { profile }) {
       state.profile = profile || {};
     }
   },
   actions: {
-    getStatus({ commit }) {
+    init({ commit, dispatch }) {
       return authApi.status().then(result => {
         if (result.isError) {
           // error
           return result;
         } else {
-          commit("gotStatus", {
+          commit('isInitialized', { initialized: true });
+          commit('isAuthenticated', {
             authenticated: result.authenticated
           });
           if (result.authenticated) {
-            commit("loggedIn", {
-              username: result.username,
-              password: undefined
-            });
+            dispatch(
+              'loggedIn',
+              {
+                username: result.username
+              },
+              { root: true }
+            );
             authApi.profile().then(result => {
               if (result.isError) {
                 // error
                 return result;
               } else {
-                commit("gotProfile", {
+                commit('setProfile', {
                   profile: result
                 });
               }
@@ -75,22 +74,25 @@ const auth = {
         }
       });
     },
-    login({ commit }, { user, pass }) {
+    login({ commit, dispatch }, { user, pass }) {
       return authApi.login(user, pass).then(result => {
         if (result.isError) {
           // error
           return result;
         } else {
-          commit("loggedIn", {
-            username: user,
-            password: pass
-          });
+          dispatch(
+            'loggedIn',
+            {
+              username: user
+            },
+            { root: true }
+          );
           authApi.profile().then(result => {
             if (result.isError) {
               // error
               return result;
             } else {
-              commit("gotProfile", {
+              commit('setProfile', {
                 profile: result
               });
             }
@@ -109,157 +111,313 @@ const auth = {
           // error
           return result;
         } else {
-          dispatch("loggedOut", null, { root: true });
+          dispatch('loggedOut', null, { root: true });
         }
       });
     },
-    update({ dispatch }, payload) {
+    initProfile({ dispatch }, payload) {
       return authApi.profile(payload).then(result => {
         if (result.isError) {
           // error
           return result;
         } else {
-          dispatch("getStatus");
+          dispatch('getStatus');
         }
       });
     }
   }
 };
 
-const search = {
-  namespaced: true,
-  state: { all: {} },
-  mutations: {
-    updateSearch(state, payload) {
-      const mode = payload.mode || "all";
-      state[mode] = {
-        qtext: getSearchParam(payload, state, "qtext"),
-        page: getSearchParam(payload, state, "page"),
-        pageLength: getSearchParam(payload, state, "pageLength"),
-        activeFacets: getSearchParam(payload, state, "activeFacets"),
-        response: getSearchParam(payload, state, "response")
-      };
+function typedSearchState(searchType) {
+  return {
+    namespaced: true,
+    state: {
+      initialized: false,
+      start: 1,
+      pageLength: 10,
+      total: 0,
+      facets: {},
+      results: [],
+      qtext: '',
+      activeFacets: {}
     },
-    toggleFacet(state, payload) {
-      const mode = payload.mode || "all";
-      const activeFacet = getSearchParam(payload, state, "activeFacets")[
-        payload.facetName
-      ];
-      if (activeFacet) {
-        const activeValue = activeFacet.values.filter(value => {
-          return value.value === payload.value;
-        });
-        if (activeValue.length) {
-          activeFacet.values = activeFacet.values.filter(value => {
-            return value.value !== payload.value;
-          });
-        } else {
-          activeFacet.values.push({
-            value: payload.value,
-            negate: payload.negate
-          });
+    mutations: {
+      isInitialized(state, { initialized }) {
+        state.initialized = initialized;
+      },
+      setResults(state, { response }) {
+        if (response.results) {
+          state.total = response.total;
+          state.results = response.results;
         }
-      } else {
-        state[mode].activeFacets[payload.facetName] = {
-          values: [{ value: payload.value, negate: payload.negate }]
+      },
+      setFacets(state, { response }) {
+        if (response.facets) {
+          state.facets = response.facets;
+        }
+      },
+      setText(state, { qtext }) {
+        if (qtext !== undefined) {
+          state.qtext = qtext;
+        }
+      },
+      setPage(state, { page }) {
+        state.start = (page - 1) * state.pageLength + 1;
+      },
+      toggleActiveFacet(state, { facet, type, value, negated }) {
+        const activeFacet = state.activeFacets[facet];
+        if (activeFacet) {
+          const activeValue = activeFacet.values.filter(facetValue => {
+            return facetValue.value === value;
+          });
+          if (activeValue.length) {
+            activeFacet.values = activeFacet.values.filter(facetValue => {
+              return facetValue.value !== value;
+            });
+          } else {
+            activeFacet.values.push({
+              value: value,
+              negated: negated
+            });
+          }
+        } else {
+          state.activeFacets[facet] = {
+            type: type,
+            values: [{ value: value, negated: negated }]
+          };
+        }
+        // FIXME? clone activeFacets to 'force' ui updates
+        state.activeFacets = {
+          ...state.activeFacets
         };
       }
+    },
+    actions: {
+      init({ commit, state }) {
+        commit('isInitialized', { initialized: true });
+        searchApi
+          .getFacets(searchType, state.qtext, state.activeFacets)
+          .then(result => {
+            if (result.response) {
+              commit('setFacets', result);
+            } else {
+              // error
+              return result;
+            }
+          });
+        return searchApi
+          .getResults(
+            searchType,
+            state.qtext,
+            state.activeFacets,
+            state.start,
+            state.pageLength
+          )
+          .then(result => {
+            if (result.response) {
+              commit('setResults', result);
+            } else {
+              // error
+              return result;
+            }
+          });
+      },
+      search({ commit, state }, { qtext }) {
+        commit('setText', { qtext });
+        searchApi
+          .getFacets(searchType, state.qtext, state.activeFacets)
+          .then(result => {
+            if (result.response) {
+              commit('setFacets', result);
+            } else {
+              // error
+              return result;
+            }
+          });
+        return searchApi
+          .getResults(
+            searchType,
+            state.qtext,
+            state.activeFacets,
+            state.start,
+            state.pageLength
+          )
+          .then(result => {
+            if (result.response) {
+              commit('setResults', result);
+            } else {
+              // error
+              return result;
+            }
+          });
+      },
+      paginate({ commit, state }, { page }) {
+        commit('setPage', { page });
+        return searchApi
+          .getResults(
+            searchType,
+            state.qtext,
+            state.activeFacets,
+            state.start,
+            state.pageLength
+          )
+          .then(result => {
+            if (result.response) {
+              commit('setResults', result);
+            } else {
+              // error
+              return result;
+            }
+          });
+      },
+      toggleFacet({ commit, state }, { facet, type, value, negated }) {
+        commit('toggleActiveFacet', { facet, type, value, negated });
+        searchApi
+          .getFacets(searchType, state.qtext, state.activeFacets)
+          .then(result => {
+            if (result.response) {
+              commit('setFacets', result);
+            } else {
+              // error
+              return result;
+            }
+          });
+        return searchApi
+          .getResults(
+            searchType,
+            state.qtext,
+            state.activeFacets,
+            state.start,
+            state.pageLength
+          )
+          .then(result => {
+            if (result.response) {
+              commit('setResults', result);
+            } else {
+              // error
+              return result;
+            }
+          });
+      }
+    },
+    getters: {
+      page(state) {
+        return Math.ceil(state.start / state.pageLength);
+      }
+    }
+  };
+}
+
+const types = ['all'];
+
+var searchModules = {};
+types.forEach(function(type) {
+  searchModules[type] = typedSearchState(type);
+});
+
+const search = {
+  namespaced: true,
+  state: {
+    initialized: false
+  },
+  modules: searchModules,
+  mutations: {
+    isInitialized(state, { initialized }) {
+      state.initialized = initialized;
     }
   },
   actions: {
-    search({ commit, rootState }, payload) {
-      return searchApi
-        .search(
-          rootState.auth.username,
-          rootState.auth.password,
-          payload.mode,
-          payload.qtext,
-          payload.page,
-          payload.length
-        )
-        .then(result => {
-          if (result.response) {
-            commit("updateSearch", result);
-          } else {
-            // error
-            return result;
-          }
-        });
-    },
-    toggleFacet({ commit, dispatch }, payload) {
-      commit("toggleFacet", payload);
-      return dispatch({});
+    init({ commit, dispatch }) {
+      types.forEach(function(type) {
+        dispatch(type + '/init');
+      });
+      commit('isInitialized', { initialized: true });
     }
   }
 };
 
-const document = {
-  namespaced: true,
-  actions: {
-    create({ rootState }, payload) {
-      return crudApi
-        .create(
-          rootState.auth.username,
-          rootState.auth.password,
-          payload.data,
-          payload.params
-        )
-        .then(result => {
-          return result;
-        });
+function typedCrudState(crudType) {
+  return {
+    namespaced: true,
+    state: {
+      initialized: false
     },
-    read({ rootState }, payload) {
-      return crudApi
-        .read(rootState.auth.username, rootState.auth.password, payload)
-        .then(result => {
-          return result;
-        });
+    mutations: {
+      isInitialized(state, { initialized }) {
+        state.initialized = initialized;
+      }
     },
-    update({ rootState }, payload) {
-      return crudApi
-        .update(
-          rootState.auth.username,
-          rootState.auth.password,
-          payload.data,
-          payload.params
-        )
-        .then(result => {
-          return result;
-        });
-    },
-    delete({ rootState }, payload) {
-      return crudApi
-        .delete(
-          rootState.auth.username,
-          rootState.auth.password,
-          payload.data,
-          payload.params
-        )
-        .then(result => {
-          return result;
-        });
+    actions: {
+      init({ commit }) {
+        commit('isInitialized', { initialized: true });
+      },
+      view(context, { id, view }) {
+        return crudApi.view(crudType, id, view);
+      },
+      create(context, { id, data, format }) {
+        return crudApi.create(crudType, id, data, format);
+      },
+      read(context, { id }) {
+        return crudApi.read(crudType, id);
+      },
+      update(context, { id, data, format }) {
+        return crudApi.update(crudType, id, data, format);
+      },
+      delete(context, { id }) {
+        return crudApi.delete(crudType, id);
+      }
     }
-  }
-};
-
-function getSearchParam(payload, state, param) {
-  const mode = payload.mode || "all";
-  const searchState = state[mode];
-  return payload[param] || (searchState && searchState[param]) || {};
+  };
 }
+
+var crudModules = {};
+types.forEach(function(type) {
+  crudModules[type] = typedCrudState(type);
+});
+
+const crud = {
+  namespaced: true,
+  state: {
+    initialized: false
+  },
+  modules: crudModules,
+  mutations: {
+    isInitialized(state, { initialized }) {
+      state.initialized = initialized;
+    }
+  },
+  actions: {
+    init({ commit, dispatch }) {
+      types.forEach(function(type) {
+        dispatch(type + '/init');
+      });
+      commit('isInitialized', { initialized: true });
+    }
+  }
+};
 
 export default new Vuex.Store({
   strict: debug,
-  state: {},
+  state: {
+    initialized: false
+  },
   mutations: {},
   actions: {
-    loggedOut({ commit }) {
-      commit("auth/loggedOut");
+    init({ dispatch }) {
+      dispatch('auth/init');
+    },
+    loggedIn({ commit, dispatch }, payload) {
+      commit('auth/loggedIn', payload);
+      dispatch('search/init');
+      dispatch('crud/init');
+    },
+    loggedOut({ commit }, payload) {
+      commit('auth/loggedOut', payload);
     }
   },
   modules: {
     auth,
     search,
-    document
+    crud
   }
 });
