bdb85cf341abe6044d4b9248704ab04965390d65
Geert Josten <geert.josten@marklogic.com>
2018-06-24 19:56:04 +0200

    Pass through facet type in ml-facets

--- src/components/ml-search/ml-facets.vue
+++ src/components/ml-search/ml-facets.vue
@@ -6,13 +6,13 @@
         v-if="hasNonSelectedValues(facet)" v-show="!facet.hide">
       <h3>{{ facetName }}</h3>
       <div v-for="(value, $index) in facet.facetValues" :key="$index" v-if="!value.selected">
-        <span v-on:click.prevent="toggle(facetName, value.name)">
+        <span v-on:click.prevent="toggle(facetName, facet.type, value.name)">
           <i class="fa fa-plus-circle facet-add-pos"></i>
           <span v-if="!!value.name" :title="value.name"> {{ value.name }}</span>
           <em v-if="!value.name">blank</em>
           <span> ({{ value.count }}) </span>
         </span>
-        <i v-if="!!negate" class="fa fa-ban facet-add-neg" v-on:click.prevent="negate(facetName, value.name)" :title="value.name"></i>
+        <i v-if="!!negate" class="fa fa-ban facet-add-neg" v-on:click.prevent="negate(facetName, facet.type, value.name)" :title="value.name"></i>
       </div>
       <div v-if="!!showMore && !facet.displayingAll">
         <a href v-on:click.prevent="showMore(facetName)">see more ...</a>
