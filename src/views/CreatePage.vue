b80951145ef463f5df5dd072c99f718543d3415e
Geert Josten <geert.josten@marklogic.com>
2018-06-24 19:57:54 +0200

    Adjusted CreatePage to work with crud types, disabled xml/json toggle

--- src/views/CreatePage.vue
+++ src/views/CreatePage.vue
@@ -184,7 +184,7 @@
             <input type="checkbox" v-model="person.isActive"/>
           </div>
         </div>
-        <div class="form-group">
+        <!--div class="form-group">
           <label class="col-sm-2 control-label required">Document Format</label>
           <div class="col-sm-1">
             <label class="radio-inline"><input type="radio" name="docFormat" value="json" v-model="person.docFormat" placeholder=""
@@ -194,7 +194,7 @@
             <label class="radio-inline"><input type="radio" name="docFormat" value="xml" v-model="person.docFormat" placeholder=""
               :disabled="mode === 'edit'" v-on:input="$v.person.docFormat.$touch">XML</label>
           </div>
-        </div>
+        </div-->
         <div class="row form-error" ng-if="$v.person.docFormat.$invalid">
           <div class="col-md-10 col-md-offset-2 has-error has-feedback" >
             <div>
@@ -214,187 +214,214 @@
 </template>
 
 <script>
-  import * as X2JS from "x2js";
-  import { required, minLength, maxLength, minValue, maxValue, numeric, email } from 'vuelidate/lib/validators';
-  import Editor from '@tinymce/tinymce-vue';
-  import * as uuid from 'uuid';
+import * as X2JS from 'x2js';
+import {
+  required,
+  minLength,
+  maxLength,
+  minValue,
+  maxValue,
+  numeric,
+  email
+} from 'vuelidate/lib/validators';
+import Editor from '@tinymce/tinymce-vue';
+import * as uuid from 'uuid';
 
-  const x2js = new X2JS();
+const x2js = new X2JS();
 
-  export default {
-    name: "CreatePage",
-    components: {
-      Editor
-    },
-    data() {
-      return {
-        person : this.initPerson(),
-        editorOptions: {
-          plugins: 'advlist autolink link image lists charmap print preview'
-        },
-        newTag: null
-      }
-    },
-    computed: {
-      profile() {
-        return this.$store.state.auth.profile || {};
+export default {
+  name: 'CreatePage',
+  components: {
+    Editor
+  },
+  props: ['type', 'id'],
+  data() {
+    return {
+      person: this.initPerson(),
+      editorOptions: {
+        plugins: 'advlist autolink link image lists charmap print preview'
       },
-      uri() {
-        return this.$route.params.uri;
-      },
-      mode() {
-        if(this.uri) {
-          return "edit";
-        } else {
-          return "create";
-        }
-      }
+      newTag: null
+    };
+  },
+  computed: {
+    profile() {
+      return this.$store.state.auth.profile || {};
     },
-    validations: {
-      person : {
-        name: {
+    mode() {
+      if (this.id) {
+        return 'edit';
+      } else {
+        return 'create';
+      }
+    }
+  },
+  validations: {
+    person: {
+      name: {
+        required,
+        minLength: minLength(3),
+        maxLength: maxLength(100)
+      },
+      age: {
+        numeric
+      },
+      eyeColor: {
+        required
+      },
+      gender: {
+        required
+      },
+      email: {
+        email
+      },
+      location: {
+        latitude: {
           required,
-          minLength: minLength(3),
-          maxLength: maxLength(100)
+          minValue: minValue(-90),
+          maxValue: maxValue(90)
         },
-        age: {
-          numeric
-        },
-        eyeColor: {
-          required
-        },
-        gender: {
-          required
-        },
-        email: {
-          email
-        },
-        location : {
-          latitude: {
-            required,
-            minValue: minValue(-90),
-            maxValue: maxValue(90)
-          },
-          longitude: {
-            required,
-            minValue: minValue(-180),
-            maxValue: maxValue(180)
-          }
-        },
-        docFormat: {
-          required
+        longitude: {
+          required,
+          minValue: minValue(-180),
+          maxValue: maxValue(180)
         }
+      },
+      docFormat: {
+        required
       }
-    },
-    methods: {
-      initPerson() {
-        if (this.uri) {
-          return this.$store.dispatch("document/get", {uri: this.uri})
+    }
+  },
+  methods: {
+    initPerson() {
+      if (this.id) {
+        return this.$store
+          .dispatch('crud/' + this.type + '/view', {
+            id: this.id,
+            view: 'metadata'
+          })
           .then(result => {
-            if (result.response) {
-              var doc = result.response;
-              var person = null;
-              if (doc.config.params.format === 'json') {
-                person = doc.data;
-              } else {
-                var wrapped = x2js.xml2js(doc.data);
-                person = wrapped.xml;
-              }
-              if (!person.tags) {
-                person.tags = [];
-              }
-              if (!person.location) {
-                person.location = {
-                  latitude: 0,
-                  longitude: 0
-                };
-              }
+            if (!result.isError) {
+              var metadata = JSON.parse(result.response);
+              return this.$store
+                .dispatch('crud/' + this.type + '/read', { id: this.id })
+                .then(result => {
+                  if (!result.isError) {
+                    var doc = result.response;
+                    var person = null;
+                    if (metadata.format === 'json') {
+                      person = JSON.parse(doc);
+                    } else {
+                      person = x2js.xml2js(doc);
+                      if (person.xml) {
+                        person = person.xml;
+                      }
+                    }
+                    if (!person.tags) {
+                      person.tags = [];
+                    }
+                    if (!person.location) {
+                      person.location = {
+                        latitude: 0,
+                        longitude: 0
+                      };
+                    }
+                  } else {
+                    // error
+                    return null;
+                  }
+                });
             } else {
               // error
               return null;
             }
           });
-        } else {
-          return {
-            name: null,
-            about: null,
-            greeting: null,
-            balance: 0,
-            picture: 'http://placehold.it/32x32',
-            age: 0,
-            eyeColor: null,
-            gender: null,
-            company: null,
-            email: null,
-            phone: null,
-            address: null,
-            location: {
-              latitude: 0,
-              longitude: 0
-            },
-            tags: [],
-            isActive: true,
-            docFormat: 'json'
-          };
-        }
-      },
-      addTag() {
-        if (this.newTag && this.newTag !== '' && this.person.tags.indexOf(this.newTag) < 0) {
-          this.person.tags.push(this.newTag);
-        }
-        this.newTag = null;
-      },
-      removeTag(index) {
-        this.person.tags.splice(index, 1);
-      },
-      submit() {
-        if(this.$v.$invalid) {
-          return;
-        }
-        const toast = this.$parent.$refs.toast;
-        var extension = '.json';
-        var data = this.person;
-        if (this.person.docFormat === 'xml') {
-          extension = '.xml';
-          var wrap = {
-            xml: this.person
-          };
-          data = x2js.js2xml(wrap);
-        }
-        if (this.mode === 'create') {
-          var uri = '/content/' + uuid.v1() + extension;
-          return this.$store.dispatch("document/create", {
-            data : data,
-            params : {
-              uri : uri,
-              format : this.person.docFormat,
-              collection : ['data', 'data/people']
-            }
-          }).then(response => {
-            if(response.isError) {
+      } else {
+        return {
+          name: null,
+          about: null,
+          greeting: null,
+          balance: 0,
+          picture: 'http://placehold.it/32x32',
+          age: 0,
+          eyeColor: null,
+          gender: null,
+          company: null,
+          email: null,
+          phone: null,
+          address: null,
+          location: {
+            latitude: 0,
+            longitude: 0
+          },
+          tags: [],
+          isActive: true,
+          docFormat: 'json'
+        };
+      }
+    },
+    addTag() {
+      if (
+        this.newTag &&
+        this.newTag !== '' &&
+        this.person.tags.indexOf(this.newTag) < 0
+      ) {
+        this.person.tags.push(this.newTag);
+      }
+      this.newTag = null;
+    },
+    removeTag(index) {
+      this.person.tags.splice(index, 1);
+    },
+    submit() {
+      if (this.$v.$invalid) {
+        return;
+      }
+      const toast = this.$parent.$refs.toast;
+      var extension = '.json';
+      var data = this.person;
+      // if (this.person.docFormat === 'xml') {
+      //   extension = '.xml';
+      //   var wrap = {
+      //     xml: this.person
+      //   };
+      //   data = x2js.js2xml(wrap);
+      // }
+      if (this.mode === 'create') {
+        return this.$store
+          .dispatch('crud/' + this.type + '/create', {
+            data,
+            format: this.person.docFormat
+          })
+          .then(response => {
+            if (response.isError) {
               toast.showToast(response.error, { theme: 'error' });
             } else {
               toast.showToast('Created', { theme: 'success' });
-              this.$router.push({name: "root.view", params: { uri: uri }});
+              this.$router.push({
+                name: 'root.view',
+                params: { id: response.id }
+              });
             }
           });
-        } else {
-          // use update when in update mode
-          return this.$store.dispatch("document/update", {
-            data : data,
-            params : {
-              uri: this.uri
+      } else {
+        // use update when in update mode
+        return this.$store
+          .dispatch('crud/' + this.type + '/update', {
+            id: this.id,
+            data,
+            format: this.person.docFormat
+          })
+          .then(response => {
+            if (response.isError) {
+              toast.showToast(response.error, { theme: 'error' });
+            } else {
+              toast.showToast('Saved', { theme: 'success' });
+              this.$router.push({ name: 'root.view', params: { id: this.id } });
             }
-          }).then(response => {
-              if(response.isError) {
-                  toast.showToast(response.error, { theme: 'error' });
-              } else {
-                  toast.showToast('Saved', { theme: 'success' });
-                  this.$router.push({ name: "root.view", params: { uri: this.uri }});
-              }
           });
-        }
       }
     }
   }
+};
 </script>
