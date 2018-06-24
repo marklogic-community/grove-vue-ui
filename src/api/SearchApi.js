da2a511d1ed6e5cdad348878f9de782df659aa4a
Geert Josten <geert.josten@marklogic.com>
2018-06-24 19:47:57 +0200

    Synch vue search api with search spec

--- src/api/SearchApi.js
+++ src/api/SearchApi.js
@@ -1,45 +1,119 @@
-import $http from "axios";
+import { polyfill } from 'es6-promise';
+import 'isomorphic-fetch';
+
+polyfill();
 
 export default {
-  name: "SearchApi",
-  search(user, pass, mode, qtext, page, pageLength, activeFacets) {
-    user = "" + user;
-    pass = "" + pass;
-    mode = mode !== undefined ? mode : "all";
-    qtext = qtext !== undefined ? qtext : "";
-    page = page || 1;
-    pageLength = pageLength || 10;
+  name: 'SearchApi',
 
-    for (var facetName in activeFacets) {
-      if (activeFacets.hasOwnProperty(facetName)) {
-        var facetValue = activeFacets[facetName];
-        qtext +=
-          (facetValue.negate ? " -" : " ") + facetName + ":" + facetValue.value;
+  getFacets(searchType, qtext, activeFacets) {
+    searchType = searchType !== undefined ? searchType : 'all';
+    qtext = qtext !== undefined ? qtext : '';
+
+    var facets = Object.keys(activeFacets || {}).map(function(facetName) {
+      var constraintType = activeFacets[facetName].type;
+      if (constraintType && constraintType.substring(0,3) === 'xs:') {
+        constraintType = 'range';
       }
-    }
+      return {
+        type: 'selection',
+        constraint: facetName,
+        constraintType: constraintType,
+        mode: 'and',
+        value: activeFacets[facetName].values.map(function(facetValue) {
+          if (facetValue.negated) {
+            return { not: facetValue.value };
+          } else {
+            return facetValue.value;
+          }
+        })
+      };
+    });
 
-    return $http({
-      method: "GET",
-      url: "/v1/search",
-      auth: {
-        username: "" + user,
-        password: "" + pass,
-        sendImmediately: true
+    return fetch('/api/search/' + searchType, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
       },
-      params: {
-        q: qtext,
-        start: (page - 1) * pageLength + 1,
-        pageLength: pageLength,
-        options: mode
-      }
+      body: JSON.stringify({
+        filters: {
+          and: [
+            {
+              type: 'queryText',
+              value: qtext
+            }
+          ].concat(facets)
+        },
+        options: {
+          start: 1,
+          pageLength: 0
+        }
+      }),
+      credentials: 'same-origin'
     }).then(
       response => {
-        response.data.results.forEach(function(result) {
-          if (! result.id) {
-            result.id = encodeURIComponent(result.uri);
-          }
+        return response.json().then(function(json) {
+          return { response: json };
         });
-        return { response: response.data };
+      },
+      error => {
+        return error;
+      }
+    );
+  },
+  getResults(searchType, qtext, activeFacets, start, pageLength) {
+    searchType = searchType !== undefined ? searchType : 'all';
+    qtext = qtext !== undefined ? qtext : '';
+    start = start || 1;
+    pageLength = pageLength || 10;
+
+    var facets = Object.keys(activeFacets || {}).map(function(facetName) {
+      var constraintType = activeFacets[facetName].type;
+      if (constraintType && constraintType.substring(0,3) === 'xs:') {
+        constraintType = 'range';
+      }
+      return {
+        type: 'selection',
+        constraint: facetName,
+        constraintType: constraintType,
+        mode: 'and',
+        value: activeFacets[facetName].values.map(function(facetValue) {
+          if (facetValue.negated) {
+            return { not: facetValue.value };
+          } else {
+            return facetValue.value;
+          }
+        })
+      };
+    });
+
+    return fetch('/api/search/' + searchType, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        filters: {
+          and: [
+            {
+              type: 'queryText',
+              value: qtext
+            }
+          ].concat(facets)
+        },
+        options: {
+          start: start,
+          pageLength: pageLength
+        }
+      }),
+      credentials: 'same-origin'
+    }).then(
+      response => {
+        return response.json().then(function(json) {
+          return { response: json };
+        });
       },
       error => {
         return error;
