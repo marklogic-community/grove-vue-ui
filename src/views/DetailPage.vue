170ff8f43ac3ed3e2a8339b3179f94a6f2dbe9ff
Geert Josten <geert.josten@marklogic.com>
2018-06-24 20:03:08 +0200

    Enhanced DetailPage to show metadata details, preview, and raw

--- src/views/DetailPage.vue
+++ src/views/DetailPage.vue
@@ -1,35 +1,88 @@
 <template>
   <div class="row detail">
     <div class="col-sm-8 content-col">
-      <h3>{{ fileName }} ({{ contentType }})</h3>
-      <view-binary :src="viewUri" :type="contentType" :title="fileName">
-        <a slot="fallback" class="btn btn-default" :href="downloadUri" target="_blank" download>Download</a>
-      </view-binary>
-      <!--h3>{{ fileName }}</h3>
-      <simple-view-file :uri="viewUri" :content-type="contentType"
-        :download-uri="downloadUri" :file-name="fileName">
-      </simple-view-file- ->
-      <h3>{{ fileName }}</h3>
-      <view-file :uri="viewUri" :content-type="contentType"
-        :allow-modal="true" :controls="true" :download-uri="downloadUri"
-        :file-name="fileName" :show-code="false" :trust-uri="false">
-      </view-file-->
+      <h3>{{ metadata.fileName }} ({{ metadata.contentType }})</h3>
+      <div class="card">
+        <b-tabs card no-fade>
+          <b-tab title="Details" active>
+            <dl class="row">
+              <dt v-show="metadata.collections" class="col-sm-2">Collections</dt>
+              <dd v-show="metadata.collections" class="col-sm-10">
+                <span v-for="(c, $index) in metadata.collections" :key="$index">
+                  {{ c }}<span v-show="$index !== metadata.collections.length - 1">, </span>
+                </span>
+              </dd>
+
+              <dt class="col-sm-2">Content-Type</dt>
+              <dd class="col-sm-10">{{ metadata.contentType }}</dd>
+
+              <dt class="col-sm-2">File Name</dt>
+              <dd class="col-sm-10">{{ metadata.fileName }}</dd>
+
+              <dt class="col-sm-2">Format</dt>
+              <dd class="col-sm-10">{{ metadata.format }}</dd>
+
+              <dt v-show="metadata.metadataValues" class="col-sm-2">Metadata</dt>
+              <dd v-show="metadata.metadataValues" class="col-sm-10">
+                <dl class="row">
+                  <template v-for="(v, key) in metadata.metadataValues">
+                    <dt class="col-sm-2">{{ key }}</dt>
+                    <dd class="col-sm-10">{{ v }}</dd>
+                  </template>
+                </dl>
+              </dd>
+
+              <dt v-show="metadata.permissions" class="col-sm-2">Permissions</dt>
+              <dd v-show="metadata.permissions" class="col-sm-10">
+                <span v-for="(p, $index) in metadata.permissions" :key="$index">
+                  {{ p }}<span v-show="$index !== metadata.permissions.length - 1">, </span>
+                </span>
+              </dd>
+
+              <dt class="col-sm-2">Quality</dt>
+              <dd class="col-sm-10">{{ metadata.quality }}</dd>
+
+              <dt class="col-sm-2">Size</dt>
+              <dd class="col-sm-10">{{ metadata.size }} bytes</dd>
+
+              <dt class="col-sm-2">Uri</dt>
+              <dd class="col-sm-10">{{ metadata.uri }}</dd>
+            </dl>
+          </b-tab>
+          <b-tab title="Preview">
+            <div v-if="metadata.format === 'json' && json">
+              <friendly-json :json="json"></friendly-json>
+            </div>
+            <div v-else-if="metadata.format === 'xml' && raw">
+              <friendly-xml :xml="raw"></friendly-xml>
+            </div>
+            <div v-else>
+              <view-binary :src="viewUri" :type="metadata.contentType" :title="metadata.fileName">
+                <a slot="fallback" class="btn btn-default" :href="downloadUri" target="_blank" download>Download</a>
+              </view-binary>
+            </div>
+          </b-tab>
+          <b-tab title="Raw" v-if="raw">
+            <pre>{{ raw }}</pre>
+          </b-tab>
+        </b-tabs>
+      </div>
     </div>
     <div class="col-sm-4 right-col">
       <div id="buttons-detail">
         <button class="btn btn-default" ui-sref="root.search">Search</button>
         <button class="btn btn-primary"
                 v-show="!profile.disallowUpdates"
-                ui-sref="root.edit({uri: uri})">Edit</button>
+                ui-sref="root.edit({id: id})">Edit</button>
         <button class="btn btn-primary"
                 v-show="!profile.disallowUpdates"
                 v-on:click.prevent="deleteDoc()">Delete</button>
       </div>
       <br>
       <!-- show (links to) similar documents -->
-      <div class="panel panel-default similar">
-        <div class="panel-heading">Similar</div>
-        <div class="panel-body">
+      <div class="card similar">
+        <div class="card-header">Similar</div>
+        <div class="card-body">
           <ml-similar :uri="uri" limit="5"></ml-similar>
         </div>
       </div>
@@ -38,65 +91,127 @@
 </template>
 
 <script>
-import mlSimilar from "@/components/ml-similar.vue";
-import viewFile from "@/components/view-file.vue";
-import simpleViewFile from "@/components/simple-view-file.vue";
+import mlSimilar from '@/components/ml-similar.vue';
+import friendlyJson from '@/components/friendly-json.vue';
+import friendlyXml from '@/components/friendly-xml.vue';
 
 export default {
-  name: "DetailPage",
+  name: 'DetailPage',
   components: {
     mlSimilar,
-    simpleViewFile,
-    viewFile
+    friendlyJson,
+    friendlyXml
+  },
+  props: ['type', 'id'],
+  data() {
+    return {
+      metadata: {},
+      json: undefined,
+      raw: undefined
+    };
   },
   computed: {
+    uri() {
+      return this.metadata.uri || '';
+    },
     profile() {
       return this.$store.state.auth.profile || {};
     },
-    id() {
-      return this.$route.params.id;
-    },
-    uri() {
-      return decodeURIComponent(this.id);
-    },
-    fileName() {
-      return this.uri.split('/').pop();
-    },
-    contentType() {
-      return "application/" + this.uri.split('.').pop();
-    },
     viewUri() {
-      return "/api/all/" + this.id;
+      return '/api/crud/' + this.type + '/' + this.id + '?';
     },
     downloadUri() {
-      return this.viewUri + "?download=true";
-    },
-    json() {
-      return {};
+      return this.viewUri + 'download=true';
     }
+  },
+  created() {
+    var self = this;
+    this.$store
+      .dispatch('crud/' + this.type + '/view', { id: this.id, view: 'metadata' })
+      .then(function(response) {
+        if (!response.isError) {
+          var metadata = JSON.parse(response.response);
+          var permissions = [];
+          // flatten permissions for simplified display
+          metadata.permissions.forEach(function(p) {
+            p.capabilities.forEach(function(c) {
+              permissions.push(p['role-name'] + ':' + c);
+            });
+          });
+          metadata.permissions = permissions;
+          if (metadata.collections.length === 0) {
+            delete metadata.collections;
+          }
+          if (metadata.permissions.length === 0) {
+            delete metadata.permissions;
+          }
+          if (Object.keys(metadata.metadataValues).length === 0) {
+            delete metadata.metadataValues;
+          }
+          self.metadata = metadata;
+
+          if (metadata.format === 'json') {
+            self.$store
+              .dispatch('crud/' + self.type + '/read', { id: self.id })
+              .then(function(response) {
+                if (!response.isError) {
+                  self.json = JSON.parse(response.response);
+                  self.raw = JSON.stringify(
+                    self.json,
+                    null,
+                    2
+                  );
+                }
+              });
+          } else if (metadata.format === 'xml') {
+            self.$store
+              .dispatch('crud/' + self.type + '/view', { id: self.id, view: 'indent' })
+              .then(function(response) {
+                if (!response.isError) {
+                  self.raw = response.response;
+                }
+              });
+          }
+        }
+      });
   },
   methods: {
     deleteDoc() {
-      if (window.confirm("This will permanently delete " + this.fileName + ", are you sure?")) {
+      if (
+        window.confirm(
+          'This will permanently delete ' +
+            this.metadata.fileName +
+            ', are you sure?'
+        )
+      ) {
         const toast = self.$parent.$refs.toast;
         const self = this;
-        self.$store.dispatch("document/delete",self.uri).then(error => {
-          if (error) {
-            toast.showToast('Failed to delete the document', { theme: 'error' });
-          } else {
-            toast.showToast('Successfully deleted the document', { theme: 'success' });
-            self.$router.push({ name: self.previousRoute.name, params: self.previousRoute.params });
-          }
-        });
+        self.$store
+          .dispatch('crud/' + this.type + '/delete', { id: self.id })
+          .then(function(error) {
+            if (error) {
+              toast.showToast('Failed to delete the document', {
+                theme: 'error'
+              });
+            } else {
+              toast.showToast('Successfully deleted the document', {
+                theme: 'success'
+              });
+              self.$router.push({
+                name: self.previousRoute.name,
+                params: self.previousRoute.params
+              });
+            }
+          });
       }
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
-  view-binary {
-    display: block;
-    height: 600px;
-  }
+view-binary {
+  display: block;
+  height: 600px;
+}
 </style>
