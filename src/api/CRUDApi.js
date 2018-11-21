549d0baba2d82183fa49d1b8a177335d18874a57
Patrick McElwee <pmcelwee@gmail.com>
2018-11-21 11:05:59 -0500

    GROVE-429: refactor out buildCrudUrl
    And change buildUrl() to match the GraphAPI in grove-vue-visjs-graph.
    Eventually, this is a candidate for a shared library of some sort. But,
    for now, let's just duplicate it.

--- src/api/CRUDApi.js
+++ src/api/CRUDApi.js
@@ -5,14 +5,28 @@ polyfill();
 
 var api = '/api/crud/';
 
+// TODO: consider refactoring out to utils library with identical
+// function in grove-vue-visjs-graph
 function buildUrl(path, params) {
   var url = new URL(api + path, window.location.href);
   if (params) {
-    Object.keys(params).forEach(key =>
-      url.searchParams.append(key, params[key])
-    );
+    Object.keys(params).forEach(key => {
+      if (Array.isArray(params[key])) {
+        params[key].map(param => url.searchParams.append(key, param));
+      } else {
+        url.searchParams.append(key, params[key]);
+      }
+    });
   }
   return url;
+}
+
+function buildCrudUrl(crudType, id, params, view) {
+  const path =
+    crudType +
+    (id ? '/' + encodeURIComponent(id) : '') +
+    (view ? '/' + view : '');
+  return buildUrl(path, params);
 }
 
 // // copied from Angular.js
@@ -24,13 +38,10 @@ function buildUrl(path, params) {
 export default {
   name: 'CRUDApi',
   view(crudType, id, view, params) {
-    return fetch(
-      buildUrl(crudType + '/' + encodeURIComponent(id) + '/' + view, params),
-      {
-        method: 'GET',
-        credentials: 'same-origin'
-      }
-    ).then(
+    return fetch(buildCrudUrl(crudType, id, params, view), {
+      method: 'GET',
+      credentials: 'same-origin'
+    }).then(
       response => {
         return response.text().then(text => {
           return { isError: false, response: text };
@@ -42,18 +53,15 @@ export default {
     );
   },
   create(crudType, id, data, format, params) {
-    return fetch(
-      buildUrl(crudType + (id ? '/' + encodeURIComponent(id) : ''), params),
-      {
-        method: 'POST',
-        headers: {
-          'content-type':
-            'application/' + (format === 'binary' ? 'octet-stream' : format)
-        },
-        body: format === 'json' ? JSON.stringify(data) : data,
-        credentials: 'same-origin'
-      }
-    ).then(
+    return fetch(buildCrudUrl(crudType, id, params), {
+      method: 'POST',
+      headers: {
+        'content-type':
+          'application/' + (format === 'binary' ? 'octet-stream' : format)
+      },
+      body: format === 'json' ? JSON.stringify(data) : data,
+      credentials: 'same-origin'
+    }).then(
       response => {
         var id = response.headers.get('location');
         return response.text().then(text => {
@@ -70,7 +78,7 @@ export default {
     );
   },
   read(crudType, id, params) {
-    return fetch(buildUrl(crudType + '/' + encodeURIComponent(id), params), {
+    return fetch(buildCrudUrl(crudType, id, params), {
       method: 'GET',
       credentials: 'same-origin'
     }).then(
@@ -85,7 +93,7 @@ export default {
     );
   },
   update(crudType, id, data, format, params) {
-    return fetch(buildUrl(crudType + '/' + encodeURIComponent(id), params), {
+    return fetch(buildCrudUrl(crudType, id, params), {
       method: 'PUT',
       headers: {
         'content-type':
@@ -105,7 +113,7 @@ export default {
     );
   },
   delete(crudType, id, params) {
-    return fetch(buildUrl(crudType + '/' + encodeURIComponent(id), params), {
+    return fetch(buildCrudUrl(crudType, id, params), {
       method: 'DELETE',
       credentials: 'same-origin'
     }).then(
